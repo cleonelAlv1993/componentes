208156baefe2975dea6ec68013510abe9903dc3e
Jaime Leonel Alvizures González <51186267+cleonelAlv1993@users.noreply.github.com>
2023-09-01 23:41:42 -0600

    Proyecto componentes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Link, Routes } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const Component1 = () => <div className="component1">Componente 1</div>;
+const Component2 = () => <div className="component2">Componente 2</div>;
+const Component3 = () => <div className="component3">Componente 3</div>;
+
+const App = () => (
+  <BrowserRouter>
+    <div className="container">
+      <nav>
+        <ul>
+          <li className="component1">
+            <Link to="/componente1">Componente 1</Link>
+          </li>
+          <li className="component2">
+            <Link to="/componente2">Componente 2</Link>
+          </li>
+          <li className="component3">
+            <Link to="/componente3">Componente 3</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/componente1" exact component={Component1} />
+        <Route path="/componente2" component={Component2} />
+        <Route path="/componente3" component={Component3} />
+      </Routes>
     </div>
-  );
-}
+  </BrowserRouter>
+);
 
 export default App;
